420c66693c90db39d83350d76af082ab3b15646c
Shishir Khatiwada <khatiwadaccr@gmail.com>
2024-09-24 12:02:49 +0545

    basic toure of react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter,  Routes, Route, Navigate} from "react-router-dom"
+import Signup from "./pages/auth/signup/Signup"
+import Login from "./pages/auth/login/Login"
+import Home from "./pages/home/Home"
+import Sidebar from "./components/common/SideBar"
+import RightPanel from "./components/common/RightPanel"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
+import { Toaster } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
+import LoadingSpinner from "./components/common/LoadingSpinner"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {data:authUser, isLoading} = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async ()=>{
+      try {
+        const res =  await fetch("/api/auth/me")
+        const data = await res.json()
+        if(data.error) return null
+        if(!res.ok) throw new Error(data.error || "Something went wrong");
+        
+        console.log("authUser is here : "  ,data) ;
+        return data
+        
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+    retry: false
+  })
+
+  if(isLoading){
+    return (
+      <div className=" h-screes flex justify-center items-center">
+        <LoadingSpinner size="lg"/>
+      </div>
+    )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex max-w-6xl mx-auto">
+        {/* sidebar is a common component so it is outside  */}
+        <BrowserRouter>
+        { authUser && <Sidebar/>}
+        <Routes>
+          <Route path="/" element={authUser ? <Home/> : <Navigate to="/login" />}/>
+          <Route path="/login" element={!authUser ? <Login/> : <Navigate to="/"/>}/>
+          <Route path="/signup" element={!authUser ? <Signup/>: <Navigate to="/"/>}/>
+          <Route path="/notifications" element={ authUser ?  <NotificationPage/> : <Navigate to="/login"/>}/>
+          <Route path="/profile/:username" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
+        </Routes>
+        { authUser &&  <RightPanel/>}
+        <Toaster/>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
